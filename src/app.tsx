2722d62693f6706ea23ea1f1e7de5241464ab1a9
joaogabriellyra <jgabriellyra@hotmail.com>
2024-10-25 01:10:40 -0300

    almost done

--- src/app.tsx
+++ src/app.tsx
@@ -1,25 +1,14 @@
-import logo from './assets/in-orbit-logo.svg'
-import womanAndRocket from './assets/woman-and-rocket.svg'
-import { Plus } from 'lucide-react'
+import { Dialog } from './components/ui/dialog'
+import { CreateGoal } from './components/create-goal'
+import { EmptyGoals } from './components/empty-goals'
+import { Summary } from './components/summary'
 
 export function App() {
   return (
-    <>
-      <div className="h-screen flex flex-col justify-center items-center gap-8">
-        <img src={logo} alt="in orbit logo" />
-        <img src={womanAndRocket} alt="woman and a rocket" />
-        <p className="text-center max-w-80 leading-relaxed text-zinc-300">
-          Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora
-          mesmo?
-        </p>
-        <button
-          type="button"
-          className="bg-violet-500 px-4 py-2.5 flex items-center gap-2 rounded-lg text-sm text-violet-50 font-medium tracking-tight hover:bg-violet-600"
-        >
-          <Plus className="size-4" />
-          Cadastrar meta
-        </button>
-      </div>
-    </>
+    <Dialog>
+      {/* <EmptyGoals /> */}
+      <Summary />
+      {/* <CreateGoal /> */}
+    </Dialog>
   )
 }
---
+++ src/components/ui/no-goal-weekend.tsx
@@ -0,0 +1,7 @@
+export function NoGoalInTheWeekend() {
+  return (
+    <p className="text-sm leading-relaxed text-zinc-400">
+      Você ainda não completou nenhuma meta essa semana.
+    </p>
+  )
+}
